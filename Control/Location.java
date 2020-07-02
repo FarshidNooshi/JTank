cdeae8fbfee8772c3a620af5af0acb0a6ede9e13
9831065 <9831065@ceit.local>
2020-07-02 16:47:52 +0430

    Declaring the Control package

--- Control/Location.java
+++ Control/Location.java
@@ -2,25 +2,55 @@ package game.Control;
 
 import game.Process.GameMap;
 
+/**
+ * This class creates for keeping the data of the
+ * game walls.
+ * Including the starting coordinates and the ending
+ * coordinates to avoid calculations in the game processing.
+ *
+ */
 public class Location {
 
+    // The two points of the walls
     private int topX;
     private int topY;
     private int bottomX;
     private int bottomY;
 
+    /**
+     * The constructor of the Location class.
+     *
+     * @param topX the first point x coordinate
+     * @param topY the first point y coordinate
+     */
     public Location (int topX, int topY) {
         this.topX = topX;
         this.topY = topY;
-        bottomX = topX + GameMap.CHANGING_FACTOR;
-        bottomY = topY + GameMap.CHANGING_FACTOR;
+        bottomX = topX + GameMap.CHANGING_FACTOR; // We calculate the second point coordinates base on the
+        bottomY = topY + GameMap.CHANGING_FACTOR; // changing factor in the game
     }
 
+    /**
+     * This method checks if the user tank and the wall are
+     * overlapping each others or not.
+     *
+     * @param userX the tank x coordinate
+     * @param userY the tank y coordinate
+     * @return the result of overlapping
+     */
     public boolean isOverlap (int userX, int userY) {
         return ( topX < userX && bottomX > userX || topX < userX + GameMap.CHANGING_FACTOR / 2 && bottomX > userX + GameMap.CHANGING_FACTOR / 2 ) &&
                 ( topY < userY && bottomY > userY || topY < userY + GameMap.CHANGING_FACTOR / 2 && bottomY > userY + GameMap.CHANGING_FACTOR / 2 );
     }
 
+    /**
+     * This method will check if the given coordinates
+     * are the coordinates of this wall or not.
+     *
+     * @param inputX the input x coordinate
+     * @param inputY the input y coordinate
+     * @return match the wall or not
+     */
     public boolean isMatch (int inputX, int inputY) {
         return inputX == topX && inputY == topY;
     }
--- Control/LocationController.java
+++ Control/LocationController.java
@@ -1,42 +1,74 @@
 package game.Control;
 
-import game.Process.GameState;
-
 import java.util.ArrayList;
 
+/**
+ * This class known as location controller is
+ * the controller to check if the tanks and the
+ * walls are overlapping each other or not.
+ * Basically it keeps all the wall date and has
+ * methods to check overlapping.
+ *
+ */
 public class LocationController {
 
+    // The list of the walls ,each type
     private static ArrayList<Location> locations;
     private static boolean created = false;
 
+    /**
+     * The init method will create the objects
+     * required.
+     *
+     */
     public static void init() {
         locations = new ArrayList<>();
         created = true;
     }
 
+    /**
+     * A method for adding a new wall location.
+     *
+     * @param location the new location to add
+     */
     public static void add (Location location) {
         if (created)
             locations.add(location);
     }
 
+    /**
+     * This method will remove the wall that matches to
+     * the locations we give to it.
+     *
+     * @param x the x coordinate
+     * @param y the y coordinate
+     */
     public static void remove (int x, int y) {
-
+        // Check for class creations
         if (!created)
             return;
-
+        // Search for any match
         for (Location l : locations)
             if (l.isMatch(x, y)) {
                 locations.remove(l);
                 break;
             }
-
     }
 
+    /**
+     * This method iterates through the walls
+     * and will check the overlapping in tank
+     * with each wall.
+     *
+     * @param x the tank x coordinate
+     * @param y the tank y coordinate
+     * @return making overlap or not
+     */
     public static boolean check (int x, int y) {
-
+        // Check for class creations
         if (!created)
             return true;
-
+        // Search for any overlap
         for (Location l : locations)
             if( l.isOverlap(x, y) )
                 return false;
