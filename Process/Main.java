043faa81d4b86b61096f10e8bf575ca2ba720220
Farshid726 <farshidnooshi726@aut.ac.ir>
2020-07-21 19:42:01 +0430

    log and start game ordered

--- Process/Main.java
+++ Process/Main.java
@@ -2,44 +2,37 @@ package game.Process;
 
 import game.Log;
 
-import java.awt.EventQueue;
-import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.*;
 
 /**
  * Program start.
- *
  */
 public class Main {
-	
-    public static void main(String[] args) {
-		// Initialize the global thread-pool
-		ThreadPool.init();
-		
-		// Show the game menu ...
-		
-		// After the player clicks 'PLAY' ...
-		EventQueue.invokeLater(new Runnable() {
-			@Override
-			public void run() {
-				Log.run();
-//				startTheGame();
-			}
 
-			private void startTheGame() {
-				GameFrame frame = new GameFrame("Simple Ball !");
-				frame.setLocationRelativeTo(null); // put frame at center of screen
-				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-				frame.setVisible(true);
-				frame.initBufferStrategy();
-				GameMap gameMap = new GameMap();
-				gameMap.init();
-				frame.setGameMap(gameMap);
-				// Create and execute the game-loop
-				GameLoop game = new GameLoop(frame);
-				game.init();
-				ThreadPool.execute(game);
-				// and the game starts ...
-			}
-		});
+    public static void main(String[] args) {
+        // Initialize the global thread-pool
+        ThreadPool.init();
+
+        // Show the game menu ...
+
+        // After the player clicks 'PLAY' ...
+        EventQueue.invokeLater(Log::run);
+    }
+
+    public void startTheGame() {
+        GameFrame frame = new GameFrame("Simple Ball !");
+        frame.setLocationRelativeTo(null); // put frame at center of screen
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        frame.initBufferStrategy();
+        GameMap gameMap = new GameMap();
+        gameMap.init();
+        frame.setGameMap(gameMap);
+        // Create and execute the game-loop
+        GameLoop game = new GameLoop(frame);
+        game.init();
+        ThreadPool.execute(game);
+        // and the game starts ...
     }
 }
