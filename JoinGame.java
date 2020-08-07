c4bb30c1a4ef14f62d63077ff8634876a9692a36
9831065 <9831065@ceit.local>
2020-08-07 19:37:33 +0430

    Size change

--- JoinGame.java
+++ JoinGame.java
@@ -91,7 +91,7 @@ public class JoinGame extends JFrame {
         int counter = 1;
         for (GameData d : data) {
             JButton jButton = new JButton();
-            jButton.setSize(new Dimension(500, 30));
+            jButton.setSize(new Dimension(700, 30));
             jButton.setBackground(new Color(30, 90, 220));
             jButton.setForeground(Color.WHITE);
             jButton.setText(d.toString());
